da86e720395b069398558f33b525d7e8ecfb00fe
jinhyenkim01 <jinhyenkim01@gmail.com>
2025-10-27 18:56:22 +0900

    Implement car name input
    Additional restriction on naming is placed, and
    the code now rejects whitespaces and ISO control
    characters as they cannot be properly printed
    out.
    
    Also imported various packages that the code will use in the future.

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,57 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Application {
+    static boolean containsInvalidCharacter(String str) {
+        for (int i = 0; i < str.length();) {
+            int charPointer = str.codePointAt(i);
+
+            if (Character.isWhitespace(charPointer)) {
+                return true;
+            }
+            if (Character.isISOControl(charPointer)) {
+                return true;
+            }
+            i = i + Character.charCount(charPointer);
+        }
+        return false;
+    }
+
+    static boolean invalidCarNameLength(String str) {
+        if (str.isEmpty()) {
+            return true;
+        }
+        if (str.length() > 5) {
+            return true;
+        }
+        return false;
+    }
+
+    static String[] inputCarName() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNameInput = Console.readLine();
+        String[] carNames = carNameInput.split(",");
+
+        for (int i = 0; i < carNames.length; i++) {
+            if (containsInvalidCharacter(carNames[i])) {
+                throw new IllegalArgumentException("자동차 이름에 사용 불가능한 문자가 있습니다. 화이트스페이스 문자 및 ISO 제어 문자는 사용 불가능합니다.");
+            }
+            if (invalidCarNameLength(carNames[i])) {
+                throw new IllegalArgumentException("자동차 이름은 최소 1자 이상, 최대 5자 이하여야 합니다.");
+            }
+        }
+        return carNames;
+    }
+
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String[] carNames = inputCarName();
     }
 }
